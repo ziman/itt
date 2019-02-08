c95d2531d2621f414fb4dbda7bd02602b1c0135a
Matus Tejiscak <ziman@functor.sk>
2019-02-08 21:25:14 +0100

    Implement the parser.

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,6 +1,9 @@
 module Parser (readProgram) where
 
+import Prelude hiding (pi)
+import Data.Char
 import Text.Parsec
+import Control.Monad
 
 import TT
 import Inference (Evar(..))
@@ -14,5 +17,59 @@ readProgram fname = do
     code <- readFile fname
     return $ runParser term 1 fname code
 
+sp :: Parser ()
+sp = many (satisfy isSpace) *> pure ()
+
+kwd :: String -> Parser ()
+kwd s = try (string s) >> sp
+
+parens :: Parser a -> Parser a
+parens p = kwd "(" *> p <* kwd ")"
+
+nrty :: Parser (Name, Evar, Term)
+nrty = (,,) <$> name <*> evar <*> term
+
+freshEvar :: Parser Evar
+freshEvar = do
+    i <- getState
+    putState (i+1)
+    return $ EV i
+
+name :: Parser Name
+name = do
+    n <- many1 $ satisfy (\c -> isAlpha c || isDigit c || c `elem` "'-_")
+    sp
+    return $ N n 0
+
+evar :: Parser Evar
+evar =
+    (kwd ":I:" *> pure (Q I))
+    <|> (kwd ":E:" *> pure (Q E))
+    <|> (kwd ":R:" *> pure (Q R))
+    <|> (kwd ":" *> freshEvar)
+
+lam :: Parser Term
+lam = do
+    kwd "\\"
+    (n, r, ty) <- nrty
+    kwd "."
+    rhs <- term
+    return $ Lam n r ty rhs
+
+pi :: Parser Term
+pi = do
+    (n, r, ty) <- parens nrty
+    kwd "->"
+    rhs <- term
+    return $ Pi n r ty rhs
+
+atom :: Parser Term
+atom = parens term <|> (V <$> name)
+
+app :: Parser Term
+app = do
+    f:xs <- many1 atom
+    foldM (\f x -> App <$> freshEvar <*> pure f <*> pure x) f xs
+
 term :: Parser Term
-term = undefined
+term = (kwd "Type" *> pure Type) <|> lam <|> pi <|> app
