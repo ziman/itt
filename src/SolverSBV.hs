51fb0da1b84fba413ca5e12e77c60e1c9f060490
Matus Tejiscak <ziman@functor.sk>
2019-02-22 12:39:04 +0100

    Make solver solve stuff.

--- src/SolverSBV.hs
+++ src/SolverSBV.hs
@@ -3,7 +3,9 @@
 module SolverSBV (solveSBV) where
 
 import Data.SBV
+import Data.Functor
 import Data.Generics
+import Control.Monad
 import qualified Data.Set as S
 import qualified Data.Map as M
 
@@ -15,7 +17,7 @@ solveSBV cs = do
     print =<< optimizeWith defaultSMTCfg{verbose} Lexicographic (goal cs)
     return []
   where
-    verbose = False
+    verbose = True
 
 data Q_ = I_ | E_ | L_ | R_ deriving (Eq, Ord, Data, Read, Show, SymVal, HasKind)
 
@@ -52,10 +54,51 @@ goal cs = do
         , R, R, R, R
         ]
 
-    [x,y,z] <- sBools ["x", "y", "z"]
-    constrain $ pbStronglyMutexed [x,y,z]
-    maximize "gx" $ 1 * b2i x
-    maximize "gy" $ 2 * b2i y
-    maximize "gz" $ 3 * b2i z
+    let qSum = foldr add (literal I_)
+
+    mul <- declFun "q_mul" $ map sq
+        [ I, I, I, I
+        , I, E, E, E
+        , I, E, L, R
+        , I, E, R, R
+        ]
+
+    let qProd = foldr mul (literal L_)
+
+    leq <- declFun "q_leq" $ map (== 1)
+        [ 1, 1, 0, 1
+        , 0, 1, 0, 1
+        , 0, 0, 1, 0
+        , 0, 0, 0, 1
+        ]
+
+    eVars <- traverse (\v -> (v,) <$> evar v) . S.toList $ evars cs
+    let eMap = M.fromList eVars
+        ev v = eMap M.! v
+
+    forM_ (S.toList $ csEqs cs) $ \(p, q) ->
+        constrain $ ev p .== ev q
+
+    forM_ (grp $ csImpls cs) $ \(e, grps) ->
+        constrain $ qSum [qProd $ map ev g | g <- grps] `leq` ev e
+
+    minimize "nR" $ cnt R_ eVars
+    minimize "nL" $ cnt L_ eVars
+    minimize "nE" $ cnt E_ eVars
+    minimize "nI" $ cnt I_ eVars  -- just for stats :)
   where
-    b2i b = ite b 1 0 :: SInteger
+    evar :: Evar -> Symbolic SQ
+    evar (EV i) = free $ "e" ++ show i
+    evar (Q q)  = pure . literal $ sq q
+
+    grp :: [S.Set Evar :-> Evar] -> [(Evar, [[Evar]])]
+    grp is = M.toList $ M.fromListWith (++) [(e, [S.toList gs]) | (gs :-> e) <- is]
+
+    cnt :: Q_ -> [(Evar, SQ)] -> SInteger
+    cnt _ [] = literal 0
+    cnt q ((_,v):vs) = ite (v .== literal q) 1 0 + cnt q vs
+
+evars :: Constrs -> S.Set Evar
+evars Constrs{csImpls, csEqs}
+    =  mconcat [ gs <> [e] | gs :-> e <- csImpls ]
+    <> mconcat [ [p, q]    | (p, q) <- S.toList csEqs ]
